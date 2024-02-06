37d8804fbbe0cb4d365ee99a1f2ff7415e8a11cf
rinaa040 <156610253+rinaa040@users.noreply.github.com>
2024-02-06 16:45:35 +0400

    Add files via upload
    задача 3

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+@app.route("/image_mars")
+def index():
+    with open("index.html", "r", encoding="utf") as f:
+        return f.read()
+@app.route('/greeting/<username>')
+def greeting(username):
+    return f'''<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                   <link rel="stylesheet"
+                   href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                   integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                   crossorigin="anonymous">
+                    <title>Привет, {username}</title>
+                  </head>
+                  <body>
+                    <h1>Привет, {username}!</h1>
+                  </body>
+                </html>'''
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8000)
