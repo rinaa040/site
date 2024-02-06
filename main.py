345cf0b6597105d786a95bed3767073bca823648
rinaa040 <156610253+rinaa040@users.noreply.github.com>
2024-02-06 17:01:49 +0400

    Add files via upload
    задача 2

--- main.py
+++ main.py
@@ -19,7 +19,23 @@ def return_sample_page():
                     <h1>И на Марсе будут яблони цвести!</h1>
                   </body>
                 </html>"""
-
+@app.route('/promotion')
+def return_promotion():
+    return """<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <title>Привет, Марс!</title>
+                    <link rel="stylesheet" type="text/css" href="static/css/style.css" />
+                  </head>
+                  <body>
+                    <h1>Человечество вырастает из детства.<br>
+                    Человечеству мала одна планета.<br>
+                    Мы сделаем обитаемыми безжизненные пока планеты.<br>
+                    И начнем с Марса!<br>
+                    Присоединяйся!</h1>
+                  </body>
+                </html>"""
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000)
