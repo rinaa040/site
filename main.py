4974b136b73361f7f05e8fd563129e0fbec78425
rinaa040 <156610253+rinaa040@users.noreply.github.com>
2024-02-09 16:19:11 +0400

    Add files via upload
    09.02 задача "Готовимся к миссии"

--- main.py
+++ main.py
@@ -1,27 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/")
-@app.route("/image_mars")
-def index():
-    with open("index.html", "r", encoding="utf") as f:
-        return f.read()
-@app.route('/index')
-def return_sample_page():
-    return """<!doctype html>
-                <html lang="en">
-                  <head>
-                    <meta charset="utf-8">
-                    <title>Привет, Марс!</title>
-                    <link rel="stylesheet" type="text/css" href="static/css/style.css" />
-                  </head>
-                  <body>
-                    <h1>И на Марсе будут яблони цвести!</h1>
-                  </body>
-                </html>"""
-@app.route('/promotion_image')
-def return_promotion():
-    with open("index2.html", "r", encoding="utf") as f:
-        return f.read()
+@app.route('/index/<title>')
+def index(title):
+    return render_template("base.html", title=title)
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000)
