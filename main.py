1d70b898e89b64fec79c93a670d27ba2cbea2d61
rinaa040 <156610253+rinaa040@users.noreply.github.com>
2024-02-09 17:14:01 +0400

    Add files via upload
    9.02 задача "Тренировки в полёте"

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route("/")
 @app.route('/index/<title>')
 def index(title):
     return render_template("base.html", title=title)
+
+
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template("trainings.html", prof=prof)
+
+
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template("professions.html", list=list)
+
+
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000)
